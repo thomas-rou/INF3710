147680c77a6d96435a2eb7d0029e1ce00ef31e77
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-04-07 11:00:22 -0400

    Add Bird and BirdPK interfaces for bird-related functionality

---
+++ common/tables/Bird.ts
@@ -0,0 +1,12 @@
+export interface Bird {
+    scientificName: string;
+    commonName: string;
+    specieStatus: string;
+    consumeScientificName: string;
+}
+
+export enum Gender {
+    M = 'M',
+    F = 'F',
+    O = 'O'
+}
---
+++ common/tables/BirdPK.ts
@@ -0,0 +1,3 @@
+export interface BirdPK {
+    scientificName: string;
+  }
