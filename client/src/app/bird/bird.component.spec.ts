dd16468a041960988bd207b76053ca18da24c897
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-04-07 17:43:31 -0400

    Update bird component to center table, add border, and update column names

--- client/src/app/bird/bird.component.spec.ts
+++ client/src/app/bird/bird.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BirdComponent } from './bird.component';
 
-describe('HotelComponent', () => {
+describe('BirdComponent', () => {
   let component: BirdComponent;
   let fixture: ComponentFixture<BirdComponent>;
 
