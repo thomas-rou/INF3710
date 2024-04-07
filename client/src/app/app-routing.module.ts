cfa56523064185ec4cc01c12e5061ac18b463a47
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-04-07 10:59:20 -0400

    Update routing paths in app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
-import { HotelComponent } from "./hotel/hotel.component";
-import { RoomComponent } from "./room/room.component";
-import { GuestComponent } from "./guest/guest.component";
+import { BirdComponent } from "./bird/bird.component";
 
 const routes: Routes = [
   { path: "app", component: AppComponent },
-  { path: "rooms", component: RoomComponent },
-  { path: "hotels", component: HotelComponent },
-  { path: "guests", component: GuestComponent },
+  { path: "birds", component: BirdComponent },
 ];
 
 @NgModule({
